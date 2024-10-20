893743f2c5b9b037c0a651961121250343028791
Ow1Max <0w1.go.0w1@gmail.com>
2024-10-20 20:12:24 +0300

    refactor

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,12 @@ const closeMenuElement = document.querySelector('.close-menu');
 
 burgerMenuBtn.addEventListener('click', () => {
   setTimeout(() => {
+    burgerMenuElement.style.display = 'block';
     burgerMenuElement.style.animation = '1s showMenu ease-in-out forwards'
   }, 500);
 
   closeMenuElement.addEventListener('click', () => {
-    burgerMenuElement.style.animation = '2s closeMenu ease-in-out forwards'
+    burgerMenuElement.style.animation = '2s closeMenu ease-in-out forwards';
   })
 });
 
