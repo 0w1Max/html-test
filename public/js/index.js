d937ea1d9747285c3ebc92d3bddfaca887dcf51e
Ow1Max <0w1.go.0w1@gmail.com>
2024-10-20 19:55:24 +0300

    implemented animation on JS

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+const burgerMenuBtn = document.querySelector('.header__right-menu--menu');
+const burgerMenuElement = document.querySelector('.burger-menu');
+const closeMenuElement = document.querySelector('.close-menu');
+
+burgerMenuBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    burgerMenuElement.style.animation = '1s showMenu ease-in-out forwards'
+  }, 500);
+
+  closeMenuElement.addEventListener('click', () => {
+    burgerMenuElement.style.animation = '2s closeMenu ease-in-out forwards'
+  })
+});
+
